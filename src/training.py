7bde6b77e3d392b7f4d52d3a748d6dcbd58c1f8e
MartinHaring <martin.haring1106@gmail.com>
2018-03-08 08:32:52 +0100

    Remove deprecated code from training.py

--- src/training.py
+++ src/training.py
@@ -256,24 +256,6 @@ input_question = 'How are you?'
 input_question = \
     data.q_to_seq(input_question, vocab_to_int)
 
-# Pad the question until it equals the max line length
-#input_question = \
-#    input_question + [vocab_to_int['<PAD>']] * (max_line_length - len(input_question))
-
-# Add empty questions so input data is correct shape
-#batch_shell = \
-#    np.zeros((data.hparams['batch_size'], max_line_length))
-
-# Set first question to be out input question
-#answer_logits = \
-#    sess.run(inference_logits,
-#             {input_data: batch_shell,
-#              keep_prob: 1.0})[0]
-
-# Remove padding from Question and Answer
-#pad_q = vocab_to_int['<PAD>']
-#pad_a = vocab_to_int['<PAD>']
-
 loaded_graph = tf.Graph()
 with tf.Session(graph=loaded_graph) as sess:
     # Load the saved model
@@ -286,8 +268,8 @@ with tf.Session(graph=loaded_graph) as sess:
     keep_prob = loaded_graph.get_tensor_by_name('keep_prob:0')
 
     answer_logits = sess.run(logits,
-                               {input_data: [input_question],
-                                keep_prob: 1.0})[0]
+                             {input_data: [input_question],
+                              keep_prob: 1.0})[0]
 
 print('Question')
 print('  Word Ids:       {}'.format([i for i in input_question]))
