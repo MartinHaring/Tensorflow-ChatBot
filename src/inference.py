22751313c12dd471fb40a2d73e7a86ff7c8b2488
MartinHaring <martin.haring1106@gmail.com>
2018-03-14 20:42:59 +0100

    Improve inference process and its user feedback

--- src/inference.py
+++ src/inference.py
@@ -1,19 +1,16 @@
 import tensorflow as tf
 import numpy as np
+import sys
 import data
-# from tensorflow.contrib.seq2seq import sequence_loss
+from tensorflow.contrib.seq2seq import sequence_loss
 from datetime import datetime
-# from model import model_inputs, seq2seq_model
+from model import model_inputs, seq2seq_model
 
 print('Inference preparation started @ {}'.format(str(datetime.now())))
 print('Initialize vocabulary...')
 vocab_to_int, int_to_vocab = data.get_word_dicts()
 
-'''     --- Re-initialize graph to clear tf.sess ---
-print('Initialize training set...')
-sorted_questions, sorted_answers = data.get_sorted_qa()
-
-print('Initialize graph...')
+print('Re-initialize TensorFlow graph...')
 train_graph = tf.Graph()
 
 with train_graph.as_default():
@@ -31,6 +28,7 @@ with train_graph.as_default():
     # Find the shape of the input data for sequence_loss
     input_shape = tf.shape(input_data)
 
+    print('Create logits using the model...')
     # Create training and inference logits
     train_logits, inference_logits = \
         seq2seq_model(tf.reverse(input_data, [-1]),
@@ -49,7 +47,7 @@ with train_graph.as_default():
     # Create a tensor to be used for making predictions
     tf.identity(inference_logits, 'logits')
 
-    print('Optimize RNN...')
+    print('Optimize training operation...')
     with tf.name_scope('optimization'):
 
         # Loss function
@@ -75,53 +73,19 @@ with train_graph.as_default():
             optimizer.apply_gradients(capped_gradients)
 
 
-print('Initialize training parameters...')
-# Validate the training with 10% of the data
-train_valid_split = \
-    int(len(sorted_questions)*0.1)
-
-# Split questions and answers into training and validating data
-train_questions = \
-    sorted_questions[train_valid_split:]
-
-train_answers = \
-    sorted_answers[train_valid_split:]
-
-valid_questions = \
-    sorted_questions[:train_valid_split]
-
-valid_answers = \
-    sorted_answers[:train_valid_split]
-
-# Record training loss for each display step
-total_train_loss = 0
-
-# Record validation loss for saving improvements in the model
-summary_valid_loss = []
-
-# Variable to keep track of consecutive validation losses
-stop_early = 0
-
-# Modulus for checking validation loss (check when 50% and 100% are done)
-validation_check = \
-    ((len(train_questions)) // data.hparams['batch_size'] // 2) - 1
-
-
-print('Training preparation finished @ {}\n'.format(str(datetime.now())))
-
-print('Start session...')
+print('Start demo session...')
 with tf.Session(graph=train_graph) as sess:
+
+    print('Restore checkpoint into demo...')
+    if not tf.train.checkpoint_exists(data.tparams['checkpoint']):
+        sys.exit('Checkpoint not found. Stopping process.')
 
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
+    saver.restore(sess, data.tparams['checkpoint'])
 
-    print('Check if checkpoint exists...')
-    if tf.train.checkpoint_exists(data.tparams['checkpoint']):
-        print('Load checkpoint...')
-        saver.restore(sess, data.tparams['checkpoint'])
-'''
 
-print('Initialize graph...')
+print('Initialize inference graph...')
 loaded_graph = tf.Graph()
 with tf.Session(graph=loaded_graph) as sess:
 
